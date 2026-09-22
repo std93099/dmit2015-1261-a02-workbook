14c634bbdab5899a0917bce4539a10c2b0837e74
Henry Hung <supercell93099@gmail.com>
2026-09-22 01:41:41 -0600

    Adding tests

--- dmit2015-javase-demo/src/test/java/dmit2015/model/CircleTest.java
+++ dmit2015-javase-demo/src/test/java/dmit2015/model/CircleTest.java
@@ -17,4 +17,32 @@ class CircleTest {
         // Assert
         assertThat(actual).isCloseTo(78.54, within(0.01));
     }
+
+    @Test
+    void area_whenRadiusIs4_returnsExpectedArea() {
+        // Arrange
+        Circle circle = new Circle();
+        circle.setRadius(4);
+
+        // Act
+        double actual = circle.area();
+
+        //Assert
+        assertThat(actual).isCloseTo(50.27, within(0.01));
+    }
+
+    @Test
+    void constructor_setsDefaultRadiusTo1() {
+        Circle circle = new Circle();
+
+        assertThat(circle.getRadius()).isEqualTo(1.0);
+    }
+
+    @Test
+    void setRadius_updatesRadius() {
+        Circle circle = new Circle();
+        circle.setRadius(7);
+
+        assertThat(circle.getRadius()).isEqualTo(7.0);
+    }
 }
